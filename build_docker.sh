85ebfe7bb1a5128d720b4b33ec9a708efa58bf03
wxn0brP <toja.filiper@gmail.com>
2025-08-24 12:36:26 +0200

    fix: build docker

--- build_docker.sh
+++ build_docker.sh
@@ -3,20 +3,11 @@
 # Stop the script on error
 set -e
 
-echo "Removing old build files..."
-rm -rf dist gui-script/dist
-
 echo "Installing dependencies..."
-yarn
+npm install
 
 echo "Compiling TypeScript..."
-yarn build
-
-echo "Compiling GUI script..."
-cd gui-script
-yarn
-yarn build
-cd ..
+npm run build
 
 echo "Building Docker image..."
 docker build -t valtheradb-server .
